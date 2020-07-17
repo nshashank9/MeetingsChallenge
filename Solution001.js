de321d4ad35d43691656909e73fbc09af1264f74
nshashank9 <30393169+nshashank9@users.noreply.github.com>
2020-07-17 00:34:16 -0400

    Create Solution001.js

---
+++ Solution001.js
@@ -0,0 +1,80 @@
+function findAvailability(meetingDuration, ...cals) {
+  var duration = meetingDuration/60;
+  var [c1,c2] = [...cals];
+  var min,max;
+  var b = [];
+     
+  var cc1 = c1.daybounds.map( function( row ) {
+          let r = row.split(":");
+         return parseInt(r[0]) + parseInt(r[1])/60;
+  } );
+   var cc2 = c2.daybounds.map( function( row ) {
+          let r = row.split(":");
+         return parseInt(r[0]) + parseInt(r[1])/60;
+  } );
+  min = Math.max(cc1[0],cc2[0]);
+  max = Math.min(cc1[1],cc2[1]);  
+
+ const cl1 = c1.meetings.map( function( row ) {
+    return row.map( function( cell ) { 
+         let num = cell.split(':');
+         num[1] = num[1]/60; 
+         return parseInt(num[0]) + parseFloat(num[1])
+    } );
+} )
+const cl2 = c2.meetings.map( function( row ) {
+    return row.map( function( cell ) { 
+         let num = cell.split(':');
+         num[1] = num[1]/60; 
+         return parseInt(num[0]) + parseFloat(num[1])
+    } );
+} )
+
+const k = cl1.concat(cl2);
+k.sort(function(a,b){
+  return a[1]-b[1]
+}).push([max,max])
+ 
+for(i=0;i<k.length-1;i++){
+   if(k[i+1][0] - k[i][1] >= duration)
+     {
+        b.push([k[i][1],k[i+1][0]])
+        
+     }
+ }  
+   const availArray = b.map( function( row ) {
+    return row.map( function( cell ) { 
+            var d =  cell - Math.floor(cell);
+            var con = d * 60;
+            if(con>0){
+            return Math.floor(cell).toString() + ":" + con.toString();
+            } else{
+               return Math.floor(cell).toString() + ":00";
+            } 
+     } );
+} )
+
+console.log(availArray)
+    return availArray;
+}
+
+const cal1 = {
+  meetings: [
+    ['9:00', '10:30'],
+    ['12:00', '13:30'],
+    ['16:00', '18:00'],
+  ],
+  daybounds: ['9:00', '20:00'],
+};
+
+const cal2 = {
+  meetings: [
+    ['10:00', '11:30'],
+    ['12:30', '14:30'],
+    ['14:30', '15:00'],
+    ['16:00', '17:00'],
+  ],
+  daybounds: ['10:00', '18:30'],
+};
+  
+findAvailability(30, cal1, cal2);
