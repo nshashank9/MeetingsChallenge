c3d44b985edc68fac36d65e517093df82a6763d4
nshashank9 <30393169+nshashank9@users.noreply.github.com>
2020-07-17 00:35:20 -0400

    Update Solution001.js

--- Solution001.js
+++ Solution001.js
@@ -57,24 +57,3 @@ for(i=0;i<k.length-1;i++){
 console.log(availArray)
     return availArray;
 }
-
-const cal1 = {
-  meetings: [
-    ['9:00', '10:30'],
-    ['12:00', '13:30'],
-    ['16:00', '18:00'],
-  ],
-  daybounds: ['9:00', '20:00'],
-};
-
-const cal2 = {
-  meetings: [
-    ['10:00', '11:30'],
-    ['12:30', '14:30'],
-    ['14:30', '15:00'],
-    ['16:00', '17:00'],
-  ],
-  daybounds: ['10:00', '18:30'],
-};
-  
-findAvailability(30, cal1, cal2);
